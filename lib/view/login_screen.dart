524c1d920aed48c27d3878d150bc219eba75f8fb
Waruna <86902580+WarunaDisanayaka@users.noreply.github.com>
2023-10-25 00:21:19 +0530

    Initial commit

---
+++ lib/view/login_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget{
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
